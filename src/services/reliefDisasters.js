ca64259c50ac4c1bd86091a504f4d730773ccbb5
CLiu <callumliu09@hotmail.co.uk>
2025-06-08 14:02:58 +0100

    Homepage creation with reliefweb stats and charts

--- src/services/reliefDisasters.js
+++ src/services/reliefDisasters.js
@@ -4,7 +4,16 @@ const BASE_URL = import.meta.env.VITE_API_BASE_URL
 
 export const getDisasters = async () => {
     try {
-        return axios.get(`${BASE_URL}/reliefweb/disasters/`)
+        return await axios.get(`${BASE_URL}/reliefweb/disasters/`)
+    } catch (error) {
+        console.log(error);
+        throw error
+    }
+}
+
+export const getDisasterStats = async () => {
+    try {
+        return await axios.get(`${BASE_URL}/reliefweb/stats/`)
     } catch (error) {
         console.log(error);
         throw error
