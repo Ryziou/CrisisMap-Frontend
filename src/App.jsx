da67dc242715659ee2f4f15c0ddd8a8185171433
CLiu <callumliu09@hotmail.co.uk>
2025-06-12 11:34:58 +0100

    Footer added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Home from './components/Home/Home'
 import Authenticate from './components/Authenticate/Authenticate'
 import UserProfile from './components/UserProfile/UserProfile'
 import UserProfileEdit from './components/UserProfileEdit/UserProfileEdit'
+import Footer from './components/Footer/Footer'
 
 
 function App() {
@@ -13,15 +14,20 @@ function App() {
 
   return (
     <>
-    <Navbar />
-    <Routes>
-      <Route path='/' element={<Home />}/>
-      <Route path='/map' element={<Map />}/>
-      <Route path='/authenticate' element={<Authenticate />}/>
-      <Route path='/profile' element={<UserProfile />}/>
-      <Route path='/profile/:userId' element={<UserProfile />}/>
-      <Route path='/profile/edit' element={<UserProfileEdit />} />
-    </Routes>
+      <div className="app-container">
+        <Navbar />
+        <main className="main-content">
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/map' element={<Map />} />
+            <Route path='/authenticate' element={<Authenticate />} />
+            <Route path='/profile' element={<UserProfile />} />
+            <Route path='/profile/:userId' element={<UserProfile />} />
+            <Route path='/profile/edit' element={<UserProfileEdit />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </>
   )
 }
